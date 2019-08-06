84d9f7171d6a64c23d980d076f2ba89854debaf9
Herb Caudill <herb@devresults.com>
2019-08-06 10:57:02 +0200

    index.js:

--- index.js
+++ index.js
@@ -2,45 +2,58 @@
 
 const util = require('util')
 const Automerge = require('automerge')
+const uuid = require('uuid')
 
-function profileNumInserts(numInserts)
-{
-  let start, duration
-  let doc = Automerge.init()
-  doc = Automerge.change(doc, '', doc => {
-    doc.x = []
+const record = {
+  first: 'Herb',
+  last: 'Caudill',
+  email: 'herb@devresults.com',
+  company: 'DevResults',
+  address: {
+    street: 'Carrer de València',
+    houseNumber: '293',
+    floor: 3,
+    door: 2,
+    postalCode: '08009',
+    city: 'Barcelona',
+    country: 'es',
+  },
+}
+
+function profileNumInserts(N) {
+  let start
+  let doc = Automerge.from({
+    list: [],
+    map: {},
   })
-  
-  console.log(`Profiling ${numInserts} Inserts.`)
+
+  console.log(`Inserting ${N} records`)
 
   start = new Date()
-  for (let i = 0; i < numInserts; i++) {
-    doc = Automerge.change(doc, '', doc => {
-      doc.x.push(i)
-    })  
+  for (let i = 0; i < N; i++) {
+    const id = uuid()
+    const r = { id, ...record }
+    doc = Automerge.change(doc, `${i}`, doc => {
+      doc.list.push(id)
+      doc.map[id] = r
+    })
   }
-  duration = (new Date()) - start
-  console.log(`Took: ${duration}ms.`)
-  
-  console.log(`Profiling ${numInserts} with regular array.push().`)
-  let x = []
-  start = new Date()
-  for (let i = 0; i < numInserts; i++) {
-    x.push(i)
-  }
-  duration = (new Date()) - start
-  console.log(`Took: ${duration}ms.`)
-  
-  const docinspection = util.inspect(doc, {showHidden: true, maxArrayLength: null})
-  const xInspection = util.inspect(x, {showHidden: true, maxArrayLength: null})
-  
-  console.log(`Size of doc: ${docinspection.length} bytes.`)
-  console.log(`Size of regular array: ${xInspection.length} bytes.`)
+
+  const duration = new Date() - start
+  const durationPerRecord = Math.round(duration / N, 1)
+  const docinspection = util.inspect(doc, { showHidden: true, maxArrayLength: null })
+  const docSize = Math.floor(docinspection.length / 1024)
+  const docSizePerRecord = Math.floor(docinspection.length / N)
+
+  console.log(`Took: ${duration} ms (${durationPerRecord} ms/record)`)
+  console.log(`Size of doc: ${docSize} Kb (${docSizePerRecord} B/record)`)
+
   console.log('\n---\n')
 }
 
-profileNumInserts(1)
-profileNumInserts(10)
 profileNumInserts(100)
+profileNumInserts(200)
+profileNumInserts(500)
 profileNumInserts(1000)
-profileNumInserts(10000)
+profileNumInserts(2000)
+profileNumInserts(4000)
